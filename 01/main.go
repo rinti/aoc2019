630151c05869ac812a3c46de7e41300ca1ff5dd4
Andreas Bernacca <hello@rinti.se>
2019-12-01 23:52:27 +0100

    Go format

--- 01/main.go
+++ 01/main.go
@@ -1,48 +1,44 @@
 package main
 
 import (
-    "io/ioutil"
-    "fmt"
-    "strings"
-    "math"
-    "strconv"
+	"fmt"
+	"io/ioutil"
+	"math"
+	"strconv"
+	"strings"
 )
 
 func mass_to_fuel(mass int) int {
-    return int(math.Floor(float64(mass) / 3) - 2)
+	return int(math.Floor(float64(mass)/3) - 2)
 }
 
 func total_mass_to_fuel(mass int, acc int) int {
-    var fuel int = mass_to_fuel(mass)
+	var fuel int = mass_to_fuel(mass)
 
-    if fuel <= 0 {
-        return acc
-    } else {
-        return total_mass_to_fuel(fuel, acc + fuel)
-    }
+	if fuel <= 0 {
+		return acc
+	} else {
+		return total_mass_to_fuel(fuel, acc+fuel)
+	}
 }
 
 func main() {
-    input, err := ioutil.ReadFile("./input.txt")
+	input, _ := ioutil.ReadFile("./input.txt")
 
-    if err != nil {
-        panic(err)
-    }
+	lines := strings.Split(string(input), "\n")
+	lines = lines[:len(lines)-1]
 
-    lines := strings.Split(string(input), "\n")
-    lines = lines[:len(lines)-1]
+	var total int = 0
+	var mass int
 
-    var total int = 0
-    var mass int;
+	for _, line := range lines {
+		if line == "\n" {
+			continue
+		}
+		mass, _ = strconv.Atoi(line)
+		// to solve problem 1 switch total_mass_to_fuel to mass_to_fuel without second arg
+		total = total + total_mass_to_fuel(mass, 0)
+	}
 
-    for _, line := range lines {
-        if line == "\n" {
-            continue
-        }
-        mass, _ = strconv.Atoi(line)
-        // to solve problem 1 switch total_mass_to_fuel to mass_to_fuel without second arg
-        total = total + total_mass_to_fuel(mass, 0)
-    }
-
-    fmt.Println(total)
+	fmt.Println(total)
 }
