06ab3a3a32c4914b478c9446e931b089b8cffb84
Andreas Bernacca <hello@rinti.se>
2019-12-04 21:23:21 +0100

    Day 4 Part 2

--- 04/main.ex
+++ 04/main.ex
@@ -7,12 +7,22 @@ defmodule Advent do
     str = to_string sorted_charlist
 
     is_increasing = charlist == sorted_charlist
-    has_double = Enum.any?(
-      ["11", "22", "33", "44", "55", "66", "77", "88", "99", "00"],
-      fn x -> str =~ x end
-    )
 
-    if is_increasing and has_double do
+    char_count = str |> String.graphemes() |> Enum.reduce(%{}, fn char, acc ->
+      Map.put(acc, char, (acc[char] || 0) + 1)
+    end)
+
+    elegible_counts = Enum.filter(
+        Map.values(char_count), fn c -> c < 2 || c > 1 && rem(c, 2) == 0 end
+    )
+    has_double_part2 = Enum.any?(elegible_counts, fn x -> x == 2 end)
+
+    # has_double_part1 = Enum.any?(
+    #   ["11", "22", "33", "44", "55", "66", "77", "88", "99", "00"],
+    #   fn x -> str =~ x end
+    # )
+
+    if is_increasing and has_double_part2 do
       true
     end
   end
