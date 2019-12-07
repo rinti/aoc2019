5e9487213cb1b0630d52ce6237b7853e1bedff3d
Andreas Bernacca <hello@rinti.se>
2019-12-07 20:43:38 +0100

    Day 5 part 2

--- 05/main.py
+++ 05/main.py
@@ -5,26 +5,57 @@ inputs = list(map(int, open("./input.txt").read().split(",")))
 def run_program(arr):
     i = 0
 
-    def add(x, y, z, *args):
+    def add(i, x, y, z, *args):
         arr[z] = arr[x] + arr[y]
+        return False, 4
 
-    def mult(x, y, z, *args):
+    def mult(i, x, y, z, *args):
         arr[z] = arr[x] * arr[y]
+        return False, 4
 
-    def put(x, *args):
-        print("asdfa", x)
-        arr[x] = 1
-        # arr[x] = int(input())
+    def put(i, x, *args):
+        print ("INPUT >> ")
+        arr[x] = int(input())
+        return False, 2
 
-    def printop(x, *args):
+    def printop(i, x, *args):
         print ("PRINT:")
         print(arr[x])
+        return False, 2
+
+    def jump_if_true(i, x, y, *args):
+        if arr[x] != 0:
+            return True, arr[y]
+        return False, 3
+
+    def jump_if_false(i, x, y, *args):
+        if arr[x] == 0:
+            return True, arr[y]
+        return False, 3
+
+    def lt(i, x, y, z, *args):
+        if arr[x] < arr[y]:
+            arr[z] = 1
+        else:
+            arr[z] = 0
+        return False, 4
+
+    def eq(i, x, y, z, *args):
+        if arr[x] == arr[y]:
+            arr[z] = 1
+        else:
+            arr[z] = 0
+        return False, 4
 
     OPS = {
         1: add,
         2: mult,
         3: put,
         4: printop,
+        5: jump_if_true,
+        6: jump_if_false,
+        7: lt,
+        8: eq,
     }
 
     def do_operation(op, i, x, y, z):
@@ -32,15 +63,17 @@ def run_program(arr):
         y = i + 2 if y == 1 else arr[i + 2]
         z = i + 3 if z == 1 else arr[i + 3]
 
-        OPS[op](x, y, z)
+        return OPS[op](i, x, y, z)
 
     while arr[i] != 99:
         instruction = f"{arr[i]:05}"  # Zero pad
         z, y, x, _, op = instruction
 
-        do_operation(int(op), i, int(x), int(y), int(z))
+        reset_pointer, increment = do_operation(int(op), i, int(x), int(y), int(z))
 
-        increment = 2 if int(op) in [3, 4] else 4
-        i = i + increment
+        if reset_pointer:
+            i = increment
+        else:
+            i = i + increment
 
 run_program(inputs)
